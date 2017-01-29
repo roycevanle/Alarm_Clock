81328e46d8018a3b15c9b9518c8fbe76f72f370b
Royce Le <Royce Le>
2017-01-28 17:50:36 -0800

    added more annyang commands

--- js/main.js
+++ js/main.js
@@ -8,41 +8,17 @@ if (annyang) {
       // $('#tpsreport').animate({bottom: '-100px'});
       document.getElementById("hiddenMessage").style.display = 'block';
       //alert("testing");
-    },
-    // annyang will capture anything after a splat (*) and pass it to the function.
-    // e.g. saying "Show me Batman and Robin" is the same as calling showFlickr('Batman and Robin');
-    'show me *tag': showFlickr,
+    }};
 
-    // A named variable is a one word variable, that can fit anywhere in your command.
-    // e.g. saying "calculate October stats" will call calculateStats('October');
-    'calculate :month stats': calculateStats,
+  var commands2 = {
+    'love': function() {
+      alert("Senpai Noticed You");
+    }};
 
-    // By defining a part of the following command as optional, annyang will respond to both:
-    // "say hello to my little friend" as well as "say hello friend"
-    'say hello (to my little) friend': greeting,
-
-    'test': test
-  };
-
-  var showFlickr = function(tag) {
-    var url = 'http://api.flickr.com/services/rest/?tags='+tag;
-    $.getJSON(url);
-  }
-
-  var calculateStats = function(month) {
-    $('#stats').text('Statistics for '+month);
-  }
-
-  var greeting = function() {
-    document.getElementById("hiddenMessage").style.display = 'block';
-  }
-
-  var test = function() {
-    document.getElementById("hiddenMessage").style.display = 'block';
-  }
 
   // Add our commands to annyang
   annyang.addCommands(commands);
+  annyang.addCommands(commands2);
 
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
